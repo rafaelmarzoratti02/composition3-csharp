8bdb882416c88b5e9b7b7510bad3f5f9b0218b08
=Rafael Marzoratti <rafaelmarzoratti02@gmail.com>
2022-07-12 17:03:22 -0300

    Adicionado classe Order

--- composition3-csharp/Entities/Order.cs
+++ composition3-csharp/Entities/Order.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using composition3_csharp.Entities.Enums;
 
 namespace composition3_csharp.Entities
 {
     class Order
     {
         public DateTime Momment { get; set; }
-        
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Itens{ get; set; } = new List<OrderItem>();
 
-        public List<OrderItem> Itens { get; set; } = new List<OrderItem>();
+        public Order()
+        {
+        }
 
+        public Order(DateTime momment, OrderStatus status)
+        {
+            Momment = momment;
+            Status = status;
+        }
 
+        public void AddItem(OrderItem item)
+        {
+            Itens.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Itens.Remove(item);
+        }
     }
 }
