5d076e0ce9416fc18999c5541455397443affe9d
=Rafael Marzoratti <rafaelmarzoratti02@gmail.com>
2022-07-12 13:00:18 -0300

    Adicionado Enum OrderStatus

---
+++ composition3-csharp/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace composition3_csharp.Entities.Enums
+{
+    enum OrderStatus
+    {
+        Pending_Payment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ composition3-csharp/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace composition3_csharp.Entities
+{
+    class Order
+    {
+        public DateTime Momment { get; set; }
+        
+
+        public List<OrderItem> Itens { get; set; } = new List<OrderItem>();
+
+
+    }
+}
